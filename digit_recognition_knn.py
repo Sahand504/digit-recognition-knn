e69789f209b53e8330575cddcbf1d8d539bd259e
sahand504 <asri.sahand@gmail.com>
2019-09-29 17:26:27 -0400

    dif given test with all training data

---
+++ digit_recognition_knn.py
@@ -0,0 +1,37 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATASET_DIR = os.path.join(BASE_DIR, "dataset1")
+TRAINING_DIR = os.path.join(DATASET_DIR, "training_validation")
+TEST_DIR = os.path.join(DATASET_DIR, "test")
+TEST_FILE = "C:\\Users\\asris\\Desktop\\Mycourselink\\Pattern Recognition\\Assignments\\Assignment 1\\dataset1\\test\\class_3_sample_12"
+
+with open(TEST_FILE) as tf:
+    array_test = tf.read().splitlines()
+
+array_files, labels, file_names = [], [], []
+for root, dirs, files in os.walk(TRAINING_DIR):
+    for filename in files:
+        label = str(filename).split("_")[1]
+        labels.append(label)
+        file_names.append(filename)
+        path = os.path.join(root, filename)
+        with open(path) as file:
+            array_files.append(file.read().splitlines())
+
+dif_counter = 0
+dif_counters = []
+for array_file_idx, array_file in enumerate(array_files):
+    for row_idx, row in enumerate(array_file):
+        for bit_idx, bit in enumerate(row):
+            if bit != array_test[row_idx][bit_idx]:
+                dif_counter += 1
+    dif_counters.append(dif_counter)
+    dif_counter = 0
+
+nearest_file_idx = dif_counters.index(min(dif_counters))
+print("Min difference found: " + str(min(dif_counters)))
+print("nearest file idx: " + str(nearest_file_idx))
+print("nearest file: \"" + files[nearest_file_idx] + "\"")
+print("classified as: " + labels[nearest_file_idx])
+
