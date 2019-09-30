91469677e48d4243d8c367fd642a8b530b7f142c
sahand504 <asri.sahand@gmail.com>
2019-09-30 00:56:31 -0400

    add K-Fold Cross Validation + modify data structure

--- digit_recognition_knn.py
+++ digit_recognition_knn.py
@@ -1,50 +1,49 @@
 import os
 import random
+import numpy
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_DIR = os.path.join(BASE_DIR, "dataset1")
 TRAINING_DIR = os.path.join(DATASET_DIR, "training_validation")
 TEST_DIR = os.path.join(DATASET_DIR, "test")
-TEST_FILE = "C:\\Users\\asris\\Desktop\\Mycourselink\\Pattern Recognition\\Assignments\\Assignment 1\\dataset1\\test\\class_3_sample_12"
 
-with open(TEST_FILE) as tf:
-    array_test = tf.read().splitlines()
+labels, image_matrix = {}, {}
+images = []
+K_FOLD = 5
+k = int(input("Please Insert the number of K between 1 to 11: "))
 
-array_files, labels, file_names = [], [], []
 for root, dirs, files in os.walk(TRAINING_DIR):
     for filename in files:
         label = int(str(filename).split("_")[1])
-        labels.append(label)
-        file_names.append(filename)
+        labels[filename] = label
         path = os.path.join(root, filename)
         with open(path) as file:
-            array_files.append(file.read().splitlines())
+            image_matrix[filename] = file.read().splitlines()
 
-zipped = list(zip(file_names, array_files, labels))
-random.shuffle(zipped)
-file_names, array_files, labels = zip(*zipped)
+file_names = list(labels.keys())
+random.shuffle(file_names)
+n = len(file_names)
 
-dif_counter = 0
-dif_counters = []
-for array_file_idx, array_file in enumerate(array_files):
-    for row_idx, row in enumerate(array_file):
-        for bit_idx, bit in enumerate(row):
-            if bit != array_test[row_idx][bit_idx]:
-                dif_counter += 1
-    dif_counters.append(dif_counter)
-    dif_counter = 0
+for i in range(K_FOLD):
+    test_file_names = file_names[int(n/K_FOLD*i):int(n/K_FOLD*(i+1))]
+    train_file_names = [item for item in file_names if item not in test_file_names]
 
+    for test_file_name in test_file_names:
+        print("-------------------------------------------------------------------" + test_file_name)
+        test_image_matrix = image_matrix[test_file_name]
+        diff_counters = []
+        for train_file_name in train_file_names:
+            diff_counter = 0
+            train_image_matrix = image_matrix[train_file_name]
+            # train_label = labels[train_file_name]
+            for row_idx, test_line in enumerate(test_image_matrix):
+                for column_idx, test_bit in enumerate(test_line):
+                    if test_bit != train_image_matrix[row_idx][column_idx]:
+                        diff_counter += 1
+            diff_counters.append(diff_counter)
 
-# sort by nearest neighbours
-dif_counters, file_names, array_files, labels \
-    = (list(t) for t in zip(*sorted(zip(dif_counters, file_names, array_files, labels))))
+        diff_counters, train_file_names \
+            = (list(t) for t in zip(*sorted(zip(diff_counters, train_file_names))))
 
-k = int(input("Please Insert the number of K between 1 to 11: "))
-nearest_labels = labels[:k]
-nearest_files = file_names[:k]
-nearest_file_idx = dif_counters.index(min(dif_counters))
-print("Nearest Labels: " + str(nearest_labels))
-print("Nearest Files: " + str(nearest_files))
-# print(*array_files[nearest_file_idx], sep='\n')
-print("Classified as: " + str(max(set(nearest_labels), key=nearest_labels.count)))
-
+        train_labels = [int(filename.split('_')[1]) for filename in train_file_names]
+        print(train_labels[:k])
